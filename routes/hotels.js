42ed27fda6dd7463ec54bc2afe05c2213ea248f5
ijsathi <ijsathi2001rs@gmail.com>
2022-12-27 21:19:20 +0600

    delete, err, get

--- routes/hotels.js
+++ routes/hotels.js
@@ -1,5 +1,6 @@
 import express from "express";
 import Hotel from "../models/Hotel.js";
+import { createError } from "../utils/error.js";
 
 const router = express.Router();
 
@@ -25,8 +26,39 @@ router.put("/:id", async (req, res) => {
         res.status(500).json(err);
     }
 });
+
 // DELETE 
+router.delete("/:id", async (req, res) => {
+    try {
+        await Hotel.findByIdAndDelete(req.params.id);
+        res.status(200).json("Hotel has been deleted!")
+    }
+    catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 // GET
+router.get("/:id", async (req, res) => {
+    try {
+        const hotel = await Hotel.findById(req.params.id);
+        res.status(200).json(hotel)
+    }
+    catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 // GET ALL
+router.get("/", async (req, res, next) => {
+    
+    try {
+        const hotels = await Hotel.find();
+        res.status(200).json(hotels)
+    }
+    catch (err) {
+        next(err)
+    }
+});
 
 export default router;
